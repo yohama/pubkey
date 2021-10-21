1f6e2fd921c4e4b8727a98281da04b7237e73e72
yohama <minctrq@yahoo.co.jp>
2021-10-21 23:37:25 +0900

    update: bugfix

--- run_docker_openssl.sh
+++ run_docker_openssl.sh
@@ -16,7 +16,9 @@ if [ "$(arch)" = "x86_64" ]; then
 fi
 
 if [ -n "${CONTAINER_NAME}" ]; then
-    docker run -itd --name ${CONTAINER_NAME} -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) -v $(pwd):/tmp/exec:z -v /tmp:/tmp:z --entrypoint='' --workdir="/tmp/exec" shamelesscookie/openssl:${IMG_TAG} /bin/sh
+    if [ -n "$(docker ps | grep ${CONTAINER_NAME})" ]; then
+        docker stop $(docker ps | grep openssl | sed -e "s/ .*//")
+    fi
+    docker run -itd --rm --name ${CONTAINER_NAME} -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) -v $(pwd):/tmp/exec:z -v /tmp:/tmp:z --entrypoint='' --workdir="/tmp/exec" shamelesscookie/openssl:${IMG_TAG} /bin/sh
 else
-    docker run -itd -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) -v $(pwd):/tmp/exec:z -v /tmp:/tmp:z --entrypoint='' --workdir="/tmp/exec" shamelesscookie/openssl:${IMG_TAG} /bin/sh
-fi
+    docker run -itd --rm -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) -v $(pwd):/tmp/exec:z -v /tmp:/tmp:z --entrypoint='' --workdir="/tmp/exec" shamelesscookie/openssl:${IMG_TAG} /bin/sh
